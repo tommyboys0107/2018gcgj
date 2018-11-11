0a0b3cf9ecc6683bffe2e0c23d757db0fb0e062c
劉哲瑋 <>
2018-11-11 14:50:33 +0800

    新增開頭畫面並且能轉跳到場景main 更新美術圖

---
+++ Assets/UIStartCon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIStartCon : MonoBehaviour {
+
+	public UIEventListener StartButt;
+
+	void Start () {
+		StartButt.onClick += (go) => {
+			SceneManager.LoadScene ("main");
+		};
+	}
+}
