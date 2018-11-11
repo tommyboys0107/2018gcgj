8d4a3add09ccb00339cf13c8e68cca1dfe26cf3a
hakuya8zai <hakuya8zai@gmail.com>
2018-11-11 13:07:35 +0800

    MoveGetFace(0123)

--- Assets/Scripts/move/Move.cs
+++ Assets/Scripts/move/Move.cs
@@ -8,35 +8,30 @@ public class Move : MonoBehaviour
     
 
     List<Vector2Int> CanPassList = new List<Vector2Int>();
+    List<Vector2Int> FirstList = new List<Vector2Int>();
     Vector2Int lastPos;
+    Vector2Int targetPos;
+    Vector2Int beforePos;
 
+        
 
-
-
-    void Start()
-    {
-        InvokeRepeating("Check", 0.5F, 0.5F);
-
-    }
-
-
-
-     public void Check () {
+    public void Check () {
 
         Vector2Int V2 = GameManager.instance.charManager.pos;
-
+        int k = 1;
         Vector2Int[] V2_ARROW = new Vector2Int[]{
-            V2 + Vector2Int.up,
-            V2 + Vector2Int.down,
-            V2 + Vector2Int.left,
-            V2 + Vector2Int.right
+            V2 + new Vector2Int(0,k),
+            V2 + new Vector2Int(0,-k),
+            V2 + new Vector2Int(-k,0),
+            V2 + new Vector2Int(k,0)
         };
 
         Vector3Int V0 = new Vector3Int(lastPos.x, lastPos.y, 0);
+        Vector3Int VBefore = new Vector3Int(beforePos.x, beforePos.y, 0);
 
-        CanPassList = new List<Vector2Int>();
+        CanPassList.Clear();
 
-        for (int i = 0; i < V2_ARROW.Length; i++)
+        for (int i = 0; i < V2_ARROW.Length; i++) ///判斷方向是否可前進，，計入list
         {
             Vector3Int V3 = new Vector3Int(V2_ARROW[i].x, V2_ARROW[i].y, 0);
             Cell C = GameManager.instance.mapManager.getCell(V3);
@@ -54,13 +49,82 @@ public class Move : MonoBehaviour
             CanPassList.Add(lastPos);
         }
 
+        for (; k < 11;k++)
+        {
+            for (int j = 0; j < CanPassList.Count;j++)
+            {
+                Vector3Int V4 = new Vector3Int(CanPassList[j].x, CanPassList[j].y, 0);
+                Cell D = GameManager.instance.mapManager.getCell(V4);
+                if (D.itemState != null)
+                {
+                    if (D.itemState.type == 0) ///碰到道具
+                    {
+                        FirstList.Add(CanPassList[j]);
+                    }
+                }
+            }
+            if (FirstList.Count != 0) break;
+        }
 
-        Vector2Int targetPos = CanPassList[Random.Range(0, CanPassList.Count)];
+        Vector2Int Before = targetPos;
+        
+
+        if (FirstList.Count != 0)
+        {
+            k = 1;
+            targetPos = FirstList[Random.Range(0, FirstList.Count)];
+            FirstList.Clear();
+        }
+        else if (FirstList.Count == 0)
+        {
+            k = 1;
+            targetPos = CanPassList[Random.Range(0, CanPassList.Count)];
+        }
+
+        Vector2Int After = targetPos;
 
         lastPos = V2;
-
-
+        GetFace(Before,After);
         GameManager.instance.charManager.charMoveTo(targetPos.x, targetPos.y);  //
+
+
+    }
+
+
+    public int GetFace(Vector2Int Before,Vector2Int After)
+    {
+        int finResult;
+        int xResult= 0;
+        int yResult = 0;
+        xResult = After.x - Before.x;
+        yResult = After.y - Before.y;
+        if(xResult==1 && yResult ==0)
+        {
+            ///結果為右
+            finResult = 0;
+        }
+        else if (xResult==-1 && yResult ==0)
+        {
+            ///結果為左
+            finResult = 1;
+        }
+        else if (yResult==1 && xResult ==0)
+        {
+            ///結果為上
+            finResult = 2;
+        }
+        else if (yResult==-1 && xResult ==0)
+        {
+            ///結果為下
+            finResult = 3;
+        }
+        else
+        {
+            finResult = -1;
+        }
+
+
+        return finResult;
 
 
     }
