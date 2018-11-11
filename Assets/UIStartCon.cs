a0c5283625ab581d8f6f8033683c388561396907
劉哲瑋 <>
2018-11-11 15:41:40 +0800

    新增作者名單

--- Assets/UIStartCon.cs
+++ Assets/UIStartCon.cs
@@ -6,10 +6,19 @@ using UnityEngine.SceneManagement;
 public class UIStartCon : MonoBehaviour {
 
 	public UIEventListener StartButt;
+	public UIEventListener PeopleButt;
+	public GameObject Peoples;
 
 	void Start () {
 		StartButt.onClick += (go) => {
 			SceneManager.LoadScene ("main");
+		};
+		PeopleButt.onClick += (go) => {
+			if (Peoples.gameObject.active) {
+				Peoples.gameObject.SetActive (false);
+			} else {
+				Peoples.gameObject.SetActive (true);
+			}
 		};
 	}
 }
